b065b138d2af984b40106f0091e98b34400c9bd9
skolosov <skolosov.work@gmail.com>
2023-02-28 21:12:32 +0300

    add forms

---
+++ src/pages/Layouts/BaseTableLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fluid class="pa-0 ma-0">
+    <v-row class="ma-0 pa-0">
+      <v-col cols="12" class="ma-0 pa-0">
+        <v-card color="grey-darken-3 pa-2 ma-2">
+          <slot name="actions"></slot>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="ma-0 pa-0">
+      <v-col cols="12" class="ma-0 pa-0">
+        <v-card class="pa-2 mr-2 ml-2" color="grey-darken-3">
+          <slot name="table"></slot>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <slot></slot>
+</template>
+
+<script>
+export default {
+  name: "BaseTableLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/StorageLocations/StorageLocations.vue
+++ src/pages/StorageLocations/StorageLocations.vue
@@ -1,106 +1,101 @@
 <template>
-  <v-sheet width="1600" class="mx-auto pa-8">
-    <v-row>
-      <v-col class="v-col-9">
-        <v-table
-            ref="tt"
-            class="v-table--density-comfortable border"
-            hover
-            fixed-header
-            height="800px"
+  <base-table-layout>
+    <template v-slot:actions>
+      <v-btn
+          color="success"
+          @click="toggleDialogCreate"
+      >
+        Создать место хранения
+      </v-btn>
+    </template>
+    <template v-slot:table>
+      <v-table
+          ref="tt"
+          class="v-table--density-comfortable border"
+          hover
+          fixed-header
+          height="800px"
+      >
+        <thead>
+        <tr>
+          <th v-for="column in columns" class="text-center bg-grey-darken-3">
+            {{ column }}
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="item in storageLocationsRows"
+            :key="item.id"
         >
-          <thead>
-          <tr>
-            <th v-for="column in columns" class="text-center bg-grey-darken-3">
-              {{ column }}
-            </th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr
-              v-for="item in storageLocationsRows"
-              :key="item.id"
-          >
-            <td>{{ item.id }}</td>
-            <td>{{ item.name }}</td>
-            <td></td>
-            <td class="text-center">
-              <v-btn
-                  variant="outlined"
-                  size="small"
-                  icon="mdi-text-box-edit-outline"
-                  color="success"
-                  @click="toggleDialogEdit(item)"
-              />
-            </td>
-            <td class="text-center">
-              <v-btn
-                  variant="outlined"
-                  size="small"
-                  icon="mdi-delete"
-                  color="error"
-                  @click="toggleDialogDelete(item)"
-              />
-            </td>
-          </tr>
-          </tbody>
-        </v-table>
-      </v-col>
-      <v-col class="v-col-3">
-        <v-card>
-          <v-btn
-              class="ma-4"
-              color="success"
-              @click="toggleDialogCreate"
-          >
-            Создать место хранения
-          </v-btn>
-        </v-card>
-      </v-col>
-    </v-row>
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td></td>
+          <td class="text-center">
+            <v-btn
+                variant="outlined"
+                size="small"
+                icon="mdi-text-box-edit-outline"
+                color="success"
+                @click="toggleDialogEdit(item)"
+            />
+          </td>
+          <td class="text-center">
+            <v-btn
+                variant="outlined"
+                size="small"
+                icon="mdi-delete"
+                color="error"
+                @click="toggleDialogDelete(item)"
+            />
+          </td>
+        </tr>
+        </tbody>
+      </v-table>
+    </template>
+  </base-table-layout>
 
-    <dialog-component
-        title="Создать место хранения"
-        :is-open="isOpenDialogCreate"
-        @toggle="toggleDialogCreate"
-    >
-      <template v-slot:content>
-        <storage-locations-form @form-sub="storeStorageLocations" @close="toggleDialogCreate"/>
-      </template>
-    </dialog-component>
-    <dialog-component
-        title="Изменение места хранения"
-        :is-open="isOpenDialogEdit"
-        @toggle="toggleDialogEdit"
-    >
-      <template v-slot:content>
-        <storage-locations-form :active="activeRow" @form-sub="updateStorageLocation" @close="toggleDialogEdit"/>
-      </template>
-    </dialog-component>
-    <dialog-component
-        title="Изменение места хранения"
-        :is-open="isOpenDialogEdit"
-        @toggle="toggleDialogEdit"
-    >
-      <template v-slot:content>
-        <storage-locations-form :active="activeRow" @form-sub="updateStorageLocation" @close="toggleDialogEdit"/>
-      </template>
-    </dialog-component>
-    <dialog-component
-        title="Удалить место хранения"
-        :is-open="isOpenDialogDelete"
-        variant="error"
-        @toggle="toggleDialogDelete"
-    >
-      <template v-slot:content>
-        <span>Вы точно хотите место хранения?</span>
-      </template>
-      <template v-slot:actions>
-        <v-btn variant="elevated" @click="destroyStorageLocation" color="error">Удалить</v-btn>
-        <v-btn variant="elevated" @click="toggleDialogDelete">Отмена</v-btn>
-      </template>
-    </dialog-component>
-  </v-sheet>
+  <dialog-component
+      title="Создать место хранения"
+      :is-open="isOpenDialogCreate"
+      @toggle="toggleDialogCreate"
+  >
+    <template v-slot:content>
+      <storage-locations-form @form-sub="storeStorageLocations" @close="toggleDialogCreate"/>
+    </template>
+  </dialog-component>
+  <dialog-component
+      title="Изменение места хранения"
+      :is-open="isOpenDialogEdit"
+      @toggle="toggleDialogEdit"
+  >
+    <template v-slot:content>
+      <storage-locations-form :active="activeRow" @form-sub="updateStorageLocation" @close="toggleDialogEdit"/>
+    </template>
+  </dialog-component>
+  <dialog-component
+      title="Изменение места хранения"
+      :is-open="isOpenDialogEdit"
+      @toggle="toggleDialogEdit"
+  >
+    <template v-slot:content>
+      <storage-locations-form :active="activeRow" @form-sub="updateStorageLocation" @close="toggleDialogEdit"/>
+    </template>
+  </dialog-component>
+  <dialog-component
+      title="Удалить место хранения"
+      :is-open="isOpenDialogDelete"
+      variant="error"
+      @toggle="toggleDialogDelete"
+  >
+    <template v-slot:content>
+      <span>Вы точно хотите место хранения?</span>
+    </template>
+    <template v-slot:actions>
+      <v-btn variant="elevated" @click="destroyStorageLocation" color="error">Удалить</v-btn>
+      <v-btn variant="elevated" @click="toggleDialogDelete">Отмена</v-btn>
+    </template>
+  </dialog-component>
 </template>
 
 <script>
@@ -108,10 +103,11 @@ import DialogComponent from "../../components/Dialog.vue";
 import StorageLocationsForm from "./StorageLocationsForm.vue";
 import {mapActions, mapGetters} from 'vuex';
 import LoaderComponent from "../../components/Loader.vue";
+import BaseTableLayout from "../Layouts/BaseTableLayout.vue";
 
 export default {
   name: "StorageLocations",
-  components: {LoaderComponent, StorageLocationsForm, DialogComponent},
+  components: {BaseTableLayout, LoaderComponent, StorageLocationsForm, DialogComponent},
   data() {
     return {
       isOpenDialogEdit: false,
@@ -131,7 +127,7 @@ export default {
     ...mapGetters(['storageLocationsRows']),
   },
   mounted() {
-    this.getStorageLocationsData();
+    !this.storageLocationsRows.length && this.getStorageLocationsData();
     this.getDivisionsData();
   },
   methods: {
